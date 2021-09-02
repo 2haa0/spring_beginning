1ed5c54db1ca73791d90359d4ddd3832d69c793a
햄식이◡̈⋆* <86291449+2haa0@users.noreply.github.com>
2021-09-02 18:11:46 +0900

    Add files via upload

---
+++ spring13/src/main/java/com/mega/mvc13/MemberController.java
@@ -0,0 +1,64 @@
+package com.mega.mvc13;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MemberController {
+/*
+	@RequestMapping("login.mega")
+	//login.mega랑 똑같으면 이하 메서드 불러와
+	public void login(String id, String pw) {
+		//login 괄호 안의 String은 index(부르는 jsp)의 input name과 동이해야 한다.
+	}
+*/
+	@Autowired
+	MemberDAO dao; // 스프링이 만들어놓은 싱글톤 객체의 주소를 넣어준다.
+	
+	@RequestMapping("login.mega")
+	public String login(MemberDTO memberDTO) {
+		//controller method내에 선언된 parameter는 무조건 프로토타입으로 만들어진다.
+		System.out.println(memberDTO);
+		System.out.println("싱글톤으로 만들어진 주소: " + dao);
+		boolean result = dao.login(memberDTO);
+		if(result) {
+			return "ok"; //views/ok.jsp로 가세요
+		} else {
+			return "no";
+		}
+	}
+	
+	@RequestMapping("create.do")
+	public void create(MemberDTO memberDTO) {
+		System.out.println(memberDTO);
+		dao.create(memberDTO);
+	}
+	
+	
+	
+	/*
+	 * 순서: index.jsp --> MemberController check method --> views/ok.jsp
+	 */
+	@RequestMapping("check.mega")
+	public String check(String nick) {
+		
+		if(nick.equals("superman")){
+			return "super";
+		} else {
+			return "common";
+		}
+		
+	}
+	
+	@RequestMapping("password.mega")
+	public String pass(String pass) {
+		System.out.println(pass);
+		if(pass.equals("1234")) {
+			return "pass";
+		} else {
+			return "redirect:index.jsp";
+		}
+	}
+	
+}
---
+++ spring13/src/main/webapp/index.jsp
@@ -0,0 +1,30 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+2021년 9월 2일 <br><br>
+오늘은 spring에 myBatis 설정했다.<br>
+이건 과젠데 오전에 했던 파일 복사만 하면 되니까 편하다.<br>
+오전엔 정말 멘붕이었는데...<br>
+ctrl+c, ctrl+v는 우주의 언어<br>
+
+<hr>
+<form action="password.mega">
+password : <input name ="pass"> <br>
+<button > 암호 처리 요청</button>
+</form>
+<hr>
+
+<form action="login.mega">
+id : <input name ="id" > <br>
+pw : <input name ="pw" > <br>
+<button > 로그인 처리 요청</button>
+</form>
+
+</body>
+</html>
